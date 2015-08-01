06ed559643e863520f963e8a48eb825e79e7cb09
purin39 <aaaa_9999@hotmail.co.jp>
2015-08-01 09:53:22 +0000

    lesson8.8

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,24 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
   def index
     @messages = Message.all
     @message = Message.new
+  end
+
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
 
   def create
@@ -18,5 +35,8 @@ class MessagesController < ApplicationController
   private
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
